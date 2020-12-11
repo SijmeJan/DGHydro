178ae1f95e622ade465d734eca08893d98505dee
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2020-12-11 16:22:20 +0000

    dump

--- src/simulation/simulation.cpp
+++ src/simulation/simulation.cpp
@@ -192,18 +192,12 @@ void Simulation::Dump(int nDump)
   if (!wf)
     throw std::runtime_error("Can not open output file for writing!");
 
-  std::cout << "Size of state: " << sizeof(t_state_deg)
-            << " " << sizeof(double)
-            << " " << UserSetup::nDeg << std::endl;
   for (int i = mesh->startX; i < mesh->endX; i++)
     for (int j = mesh->startY; j < mesh->endY; j++)
       for (int k = mesh->startZ; k < mesh->endZ; k++)
         for (int n = 0; n < UserSetup::nDeg; n++) {
-          std::cout << "Writing " << sizeof(t_state) << " for i = "
-                    << i << ", degree " << n << std::endl;
           t_state temp(mesh_state[0][k*mesh->Nx*mesh->Ny + j*mesh->Nx + i][n]);
           wf.write((char *) &temp[0], sizeof(t_state));
-          std::cout << "t = " << temp[0] << std::endl;
         }
 
   wf.close();
@@ -228,9 +222,6 @@ void Simulation::RestoreFromDump(int nDump)
           t_state temp(0.0);
           rf.read((char *) &temp[0], sizeof(t_state));
           mesh_state[0][k*mesh->Nx*mesh->Ny + j*mesh->Nx + i][n] = temp;
-          std::cout << "Reading " << sizeof(t_state) << " for i = "
-                    << i << ", degree " << n << std::endl;
-          std::cout << "t = " << temp[0] << std::endl;
         }
 
   rf.close();
