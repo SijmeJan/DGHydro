892d87a656c0af1ca76fdc67b3e0f3cb9136d70c
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2020-12-11 14:47:03 +0000

    dump

--- src/state/state.hpp
+++ src/state/state.hpp
@@ -27,6 +27,10 @@ namespace DGHydro {
     // Calculate state from degrees of freedom
     Array<double, nEq> U(Array<Array<double, nEq>, nDeg>& s,
                          double x, double y, double z) {
+      std::cout << "Calculating state..." << std::endl;
+      std::cout << s[0] << " " << s[1] << std::endl;
+      std::cout << "Hallo..." << std::endl;
+
       Array<double, nEq> result;
       result = s[0]*bf(0, x, y, z);
 
