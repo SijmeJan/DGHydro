01284b4d595ee8218c966c8b76d55250f05848e3
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2023-01-24 13:54:59 +0000

    Version

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,31 @@
+#include <iostream>
+
 //#include <Kokkos_Core.hpp>
+#include "DGConfig.h"
 
 int main( int argc, char* argv[] ) {
-  bool initKokkosBeforeMPI = false;
+    // Report version
+    std::cout << "Running version "
+              << DGHYDRO_VERSION_MAJOR << "."
+              << DGHYDRO_VERSION_MINOR << std::endl;
 
-  // When running on GPUS with Omnipath network,
-  // Kokkos needs to be initialised *before* the MPI layer
+    bool initKokkosBeforeMPI = false;
+
+    // When running on GPUS with Omnipath network,
+    // Kokkos needs to be initialised *before* the MPI layer
 #ifdef KOKKOS_ENABLE_CUDA
-  if(std::getenv("PSM2_CUDA") != NULL) {
-    initKokkosBeforeMPI = true;
-  }
+    if(std::getenv("PSM2_CUDA") != NULL) {
+        initKokkosBeforeMPI = true;
+    }
 #endif
 
-  //if(initKokkosBeforeMPI)  Kokkos::initialize( argc, argv );
+    //if(initKokkosBeforeMPI)  Kokkos::initialize( argc, argv );
 
 #ifdef WITH_MPI
-  MPI_Init(&argc,&argv);
+    MPI_Init(&argc,&argv);
 #endif
 
-  //if(!initKokkosBeforeMPI) Kokkos::initialize( argc, argv );
+    //if(!initKokkosBeforeMPI) Kokkos::initialize( argc, argv );
 
-  return 0;
+    return 0;
 }
